94f4739fb59c89b8000e052065d3abf11b615a66
Chris Endcliffe <chris.endcliffe@weare-awesome.co.uk>
2018-04-05 11:13:37 +0100

    Authentication exceptions added

--- src/Authentication/AuthenticationException.php
+++ src/Authentication/AuthenticationException.php
@@ -4,8 +4,12 @@ namespace WeAreAwesome\AwesomenessSDK\Authentication;
 
 class AuthenticationException extends \Exception
 {
+
     const SESSION_NOT_AUTHENTICATED_CODE = 1;
 
+    /**
+     * @return static
+     */
     public static function notAuthenticated()
     {
         return new static('Session not authenticated', self::SESSION_NOT_AUTHENTICATED_CODE);
