a43766b6961dd64addffac8df19c718846af9d45
Chris Endcliffe <chris.endcliffe@weare-awesome.co.uk>
2018-04-09 15:42:04 +0100

    contacts endpoint added

---
+++ src/Endpoints/Contacts.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace WeAreAwesome\AwesomenessSDK\Endpoints;
+
+use WeAreAwesome\AwesomenessSDK\Awesomeness;
+
+class Contacts
+{
+
+    /**
+     * @var Awesomeness $awesomeness
+     */
+    protected $awesomeness;
+
+    /**
+     * Contacts constructor.
+     *
+     * @param Awesomeness $awesomeness
+     */
+    public function __construct(Awesomeness $awesomeness)
+    {
+        $this->awesomeness = $awesomeness;
+    }
+
+    public function me()
+    {
+        return $this->
+        awesomeness
+            ->http()
+            ->sync()
+            ->get('contacts/me');
+    }
+
+    public function byId($id)
+    {
+        return $this->awesomeness
+            ->http()
+            ->sync()
+            ->get("contacts/$id");
+    }
+}
