925eff5c826ada2127076e50ba81b8a8dd5bec9c
Chris Endcliffe <chris.endcliffe@weare-awesome.co.uk>
2018-07-04 08:34:16 +0100

    video added

--- src/Lib/Content/Types/Video.php
+++ src/Lib/Content/Types/Video.php
@@ -4,8 +4,41 @@ namespace WeAreAwesome\AwesomenessSDK\Lib\Content\Types;
 
 Class Video extends ContentItem implements ContentTypeInterface
 {
+
+    /**
+     * @param $provider
+     *
+     * @return string
+     */
+    private function getWrapper(
+        $provider
+    ) {
+        switch ($provider) {
+            case 'vimeo' : {
+                return "<iframe class='Awesome__video' src='https://player.vimeo.com/video/$this->body' allow='autoplay; encrypted-media' allowFullScreen={true} ></iframe>";
+            }
+            case 'youtube' : {
+                return "<iframe class='Awesome__video' src='https://www.youtube.com/embed/$this->body' allow='autoplay; encrypted-media' allowFullScreen={true} ></iframe>";
+            }
+            default:
+                return '';
+        }
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return string|void
+     */
     public function render(array $options = [])
     {
-        return $this->body;
+        if (!$provider = $this->meta
+            ->get('provider')
+            ->getValue()
+        ) {
+            return '';
+        }
+
+        return $this->getWrapper($provider);
     }
 }
