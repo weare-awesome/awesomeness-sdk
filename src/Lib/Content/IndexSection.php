b9f976608c1db91fcec046087e293977636eed25
Chris Endcliffe <chris.endcliffe@weare-awesome.co.uk>
2018-10-17 12:34:25 +0100

    index section added

---
+++ src/Lib/Content/IndexSection.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace WeAreAwesome\AwesomenessSDK\Lib\Content;
+
+
+class IndexSection extends Section
+{
+    
+    public function getContentType() {
+        
+    }
+
+
+    public function getPerPage()
+    {
+        
+    }
+
+    public function isPaginated()
+    {
+        
+    }
+}
