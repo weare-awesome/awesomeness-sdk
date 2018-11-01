f1d7d808a481ba6842c4540d64ec25f2c6c4967f
Chris Endcliffe <chris.endcliffe@weare-awesome.co.uk>
2018-11-01 11:07:11 +0000

    initial distribution class complete

--- src/Lib/Content/DistributionPage.php
+++ src/Lib/Content/DistributionPage.php
@@ -1,15 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chris
- * Date: 01/11/18
- * Time: 10:50
- */
+
 
 namespace WeAreAwesome\AwesomenessSDK\Lib\Content;
 
 
+use WeAreAwesome\AwesomenessSDK\Http\ApiResponse;
+
 class DistributionPage extends Page
 {
+
+
+    /**
+     * @param ApiResponse $response
+     * @return DistributionPage
+     */
+    public static function makeFromApiResponse(ApiResponse $response) {
+        $page = PageFactory::makeFromArray($response->getData()['content']);
+        $distributionPage = self::castPageToDistributionPage($page);
+        return $distributionPage;
+    }
+
+    /**
+     * @param Page $page
+     * @return DistributionPage
+     */
+    public static function castPageToDistributionPage(Page $page) {
+        $distributionPage = new DistributionPage();
+        $distributionPage->setId($page->getId());
+        $distributionPage->setSlug($page->getSlug());
+        $distributionPage->setTitle($page->getTitle());
+        $distributionPage->setBody($page->getBody());
+        $distributionPage->setType($page->getType());
+        $distributionPage->setMeta($page->getMeta());
+        $distributionPage->setPath($page->getPath());
+        $distributionPage->setPublishDate($page->getPublishDate());
+        $distributionPage->setSections($page->getSections());
+        return $distributionPage;
+    }
 
 }
